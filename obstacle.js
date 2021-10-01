08362af18b7ef996b9a8ef3970c19f0edcda2f51
hoangfvi09 <huonglt129@gmail.com>
2021-10-01 13:42:59 +0700

    speed obstacle khac nhau

--- obstacle.js
+++ obstacle.js
@@ -3,6 +3,7 @@ class Obstacle {
     left;
     src;
     status;
+    speed;
 
     constructor(id) {
         this.top = -OBSTACLE_HEIGHT
@@ -10,6 +11,7 @@ class Obstacle {
         this.src = 'image/obstacle' + Math.ceil(Math.random() * 4) + '.png'
         this.id = id
         this.status=true;
+        this.speed= OBSTACLE_SPEED+Math.ceil(Math.random() * 2)
     }
 
     drawObstacle() {
@@ -23,7 +25,7 @@ class Obstacle {
             this.src = 'image/obstacle' + Math.ceil(Math.random() * 3) + '.png'
             this.left= MAP_LEFT + Math.floor(Math.random() * NUMBER_OF_LANES)*LANE_WIDTH
         } else {
-            this.top += OBSTACLE_SPEED+Math.floor(Math.random() * 3)
+            this.top += this.speed
         }
 
     }
